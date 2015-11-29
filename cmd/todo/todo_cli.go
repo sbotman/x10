633b6f9f4c4e5aaffef979176f6ad3e10ebafda0
Sander Botman <sbotman@schubergphilis.com>
2015-11-29 17:33:11 +0100

    first commit

---
+++ cmd/todo/todo_cli.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"fmt"
+	"github.com/benschw/go-todo/client"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+	"strconv"
+	"github.com/benschw/go-todo/api"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "todo cli"
+	app.Usage = "cli to work with the `todo` microservice"
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "host",
+			Value:  "http://localhost:8080",
+			Usage:  "Todo service host",
+			EnvVar: "APP_HOST",
+		},
+
+		//		cli.StringFlag{"host", "http://localhost:8080", "Todo service host", "APP_HOST"},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "add",
+			Usage: "(title description) create a todo",
+			Action: func(c *cli.Context) {
+				title := c.Args().Get(0)
+				desc := c.Args().Get(1)
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todo, err := client.CreateTodo(title, desc)
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				fmt.Printf("%+v\n", todo)
+			},
+		},
+		{
+			Name:  "ls",
+			Usage: "list all todos",
+			Action: func(c *cli.Context) {
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todos, err := client.GetAllTodos()
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				for _, todo := range todos {
+					fmt.Printf("%+v\n", todo)
+				}
+			},
+		},
+		{
+			Name:  "delete",
+			Usage: "(id) delete a todo item",
+			Action: func(c *cli.Context) {
+				idStr := c.Args().Get(0)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				if err := client.DeleteTodo(int32(id)); err != nil {
+					log.Fatal(err)
+					return
+				}
+				fmt.Println("todo", id, "deleted" )
+			},
+		},
+		{
+			Name:  "doing",
+			Usage: "(id) update a todo status to 'doing'",
+			Action: func(c *cli.Context) {
+				idStr := c.Args().Get(0)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todo, err := client.UpdateTodoStatus(int32(id), api.DoingStatus)
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				fmt.Printf("%+v\n", todo)
+			},
+		},
+		{
+			Name:  "done",
+			Usage: "(id) update a todo status to 'done'",
+			Action: func(c *cli.Context) {
+				idStr := c.Args().Get(0)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todo, err := client.UpdateTodoStatus(int32(id), api.DoneStatus)
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				fmt.Printf("%+v\n", todo)
+			},
+		},
+		{
+			Name:  "show",
+			Usage: "(id title description) show a todo title and description",
+			Action: func(c *cli.Context) {
+				idStr := c.Args().Get(0)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todo, err := client.GetTodo(int32(id))
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+				fmt.Printf("%+v\n", todo)
+			},
+		},
+		{
+			Name:  "save",
+			Usage: "(id title description) update a todo title and description",
+			Action: func(c *cli.Context) {
+				idStr := c.Args().Get(0)
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+				title := c.Args().Get(1)
+				desc := c.Args().Get(2)
+
+				host := c.GlobalString("host")
+
+				client := client.TodoClient{Host: host}
+
+				todo, err := client.GetTodo(int32(id))
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+
+				todo.Title = title
+				todo.Description = desc
+
+				todo2, err := client.UpdateTodo(todo)
+				if err != nil {
+					log.Fatal(err)
+					return
+				}
+
+				fmt.Printf("%+v\n", todo2)
+			},
+		},
+	}
+	app.Run(os.Args)
+
+}
